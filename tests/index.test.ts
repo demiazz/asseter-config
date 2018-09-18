0e6bec281d30c0d5bce381f2712036b355c843b8
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-18 23:16:16 +0300

    Fix exports from package

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,36 +1,8 @@
-import { validate } from "../src";
+import { load } from "../src";
+import { load as originalLoad } from "../src/load";
 
-import { getFixturePath } from "./helpers";
-
-describe("validate", () => {
-  describe("when configuration is valid", () => {
-    it("doesn't throw an error", () => {
-      const schemaPath = getFixturePath("index/schema.json");
-      const configurationPath = getFixturePath("index/valid.json");
-
-      expect(() => {
-        validate(schemaPath, configurationPath);
-      }).not.toThrowError();
-    });
-  });
-
-  describe("when configuration is invalid", () => {
-    it("throws an error", () => {
-      const schemaPath = getFixturePath("index/schema.json");
-      const configurationPath = getFixturePath("index/invalid.json");
-
-      expect(() => {
-        validate(schemaPath, configurationPath);
-      }).toThrowError();
-    });
-
-    it("throws an error with all errors information", () => {
-      const schemaPath = getFixturePath("index/schema.json");
-      const configurationPath = getFixturePath("index/invalid.json");
-
-      expect(() => {
-        validate(schemaPath, configurationPath);
-      }).toThrowErrorMatchingSnapshot();
-    });
+describe("exports", () => {
+  it("exports `load`", () => {
+    expect(load).toBe(originalLoad);
   });
 });
