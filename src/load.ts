bf79557f05c6e02d87954a3478bbadfb57c18661
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-18 22:34:40 +0300

    Move `RawConfiguration` type to `load` module

--- src/load.ts
+++ src/load.ts
@@ -1,9 +1,29 @@
 import { resolve } from "path";
 
-import { JSONValue } from "./json";
+import { JSONObject, JSONValue } from "./json";
 import { read } from "./read";
-import { RawConfiguration } from "./types";
 import { Errors, validate, ValidationError } from "./validate";
+
+type RawProviderOptions = JSONObject;
+
+type RawProviderEnvironmentOptions = Record<string, RawProviderOptions>;
+
+interface RawProvider extends JSONObject {
+  environment?: RawProviderEnvironmentOptions;
+  options: RawProviderOptions;
+  type: string;
+}
+
+interface RawProviders extends Record<string, RawProvider> {
+  default: RawProvider;
+}
+
+interface RawConfiguration extends JSONObject {
+  defaultEnvironment: string;
+  environmentVariable: string;
+  packageManager: "npm" | "yarn";
+  providers: RawProviders;
+}
 
 const schemaPath = resolve(__dirname, "../schema.json");
 
