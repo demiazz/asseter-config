070570f16b60c09448bed9bd8e67510cc168e36d
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-18 13:45:07 +0300

    Add validation by given path

--- src/__tests__/validate.test.ts
+++ src/__tests__/validate.test.ts
@@ -3,30 +3,105 @@ import { validate } from "../validate";
 import { getFixture, getFixturePath } from "./helpers";
 
 describe("validate", () => {
-  it("returns empty list of errors when data is valid", () => {
-    const definitionPath = getFixturePath("validate/schema.json");
-    const data = getFixture(`validate/valid.json`);
+  [
+    ["boolean", true],
+    ["null", null],
+    ["number", 10],
+    ["string", ""],
+    ["undefined", undefined],
+    ["array", []]
+  ].forEach(([type, value]) => {
+    describe(`given value of type '${type}'`, () => {
+      it("throws an error", () => {
+        const definition = getFixturePath("validate/schema.json");
 
-    expect(validate(definitionPath, data)).toEqual([]);
+        expect(() => validate(definition, value)).toThrowError(
+          "Value must be an object"
+        );
+      });
+    });
   });
 
-  it("returns list of errors when data is invalid", () => {
-    const definitionPath = getFixturePath("validate/schema.json");
-    const data = getFixture(`validate/invalid.json`);
+  describe("when given path doesn't exist", () => {
+    it("throws an error", () => {
+      const definitionPath = getFixturePath("validate/schema.json");
+      const data = {
+        bar: [{ baz: "foo" }]
+      };
 
-    expect(validate(definitionPath, data)).toEqual([
-      {
-        message: "should NOT have additional properties",
-        path: ""
-      },
-      {
-        message: "should be string",
-        path: ".foo"
-      },
-      {
-        message: "should be string",
-        path: ".bar"
-      }
-    ]);
+      expect(() => validate(definitionPath, data, ["bar", "baz"])).toThrowError(
+        "Path 'bar.baz' doesn't exists"
+      );
+    });
+  });
+
+  describe("when path is empty", () => {
+    it("returns empty list of errors when data is valid", () => {
+      const definitionPath = getFixturePath("validate/schema.json");
+      const data = getFixture("validate/valid.json");
+
+      expect(validate(definitionPath, data)).toEqual([]);
+    });
+
+    it("returns list of errors when data is invalid", () => {
+      const definitionPath = getFixturePath("validate/schema.json");
+      const data = getFixture("validate/invalid.json");
+
+      expect(validate(definitionPath, data)).toEqual([
+        {
+          message: "should NOT have additional properties",
+          path: ""
+        },
+        {
+          message: "should be string",
+          path: ".foo"
+        },
+        {
+          message: "should be string",
+          path: ".bar"
+        }
+      ]);
+    });
+  });
+
+  describe("when given path is not empty", () => {
+    it("returns empty list of errors when data by path is valid", () => {
+      const definitionPath = getFixturePath("validate/schema.json");
+      const data = {
+        firstLevel: {
+          secondLevel: getFixture("validate/valid.json")
+        }
+      };
+
+      expect(
+        validate(definitionPath, data, ["firstLevel", "secondLevel"])
+      ).toEqual([]);
+    });
+
+    it("returns list of errors when data is invalid", () => {
+      const definitionPath = getFixturePath("validate/schema.json");
+      const data = {
+        firstLevel: {
+          secondLevel: getFixture("validate/invalid.json")
+        }
+      };
+
+      expect(
+        validate(definitionPath, data, ["firstLevel", "secondLevel"])
+      ).toEqual([
+        {
+          message: "should NOT have additional properties",
+          path: ".firstLevel.secondLevel"
+        },
+        {
+          message: "should be string",
+          path: ".firstLevel.secondLevel.foo"
+        },
+        {
+          message: "should be string",
+          path: ".firstLevel.secondLevel.bar"
+        }
+      ]);
+    });
   });
 });
--- src/validate.ts
+++ src/validate.ts
@@ -1,16 +1,28 @@
+import { ErrorObject } from "ajv";
+
 import { readSchema } from "./read";
 import { JSONValue } from "./types";
+import { getByPath } from "./utils";
+
+type Errors = Array<{ message: string; path: string }>;
+
+const processErrors = (errors: ErrorObject[], path: string[]): Errors => {
+  const prepend = path.length > 0 ? `.${path.join(".")}` : "";
+
+  return errors.map(({ dataPath, message = "" }) => ({
+    message,
+    path: `${prepend}${dataPath}`
+  }));
+};
 
 export const validate = (
   definition: string,
-  data: JSONValue
-): Array<{ message: string, path: string }> => {
+  data: JSONValue,
+  path: string[] = []
+): Errors => {
   const schema = readSchema(definition);
 
-  schema(data);
+  schema(getByPath(data, path));
 
-  return (schema.errors || []).map(({ dataPath, message = "" }) => ({
-    message,
-    path: dataPath
-  }));
+  return processErrors(schema.errors || [], path);
 };
