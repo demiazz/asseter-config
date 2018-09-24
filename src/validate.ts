815c995ec698891605d66c08406e9f2d60e61fd2
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-25 00:19:14 +0300

    Remove `validate` function

--- src/validate.ts
+++ src/validate.ts
@@ -2,37 +2,6 @@ import { ValidateFunction } from "ajv";
 
 import { JSONValue } from "./types";
 
-export type Errors = Array<{ message: string; path: string }>;
-
-export const validate = (schema: ValidateFunction, data: JSONValue): Errors => {
-  schema(data);
-
-  if (!schema.errors) {
-    return [];
-  }
-
-  const errors: Errors = [];
-  const hash: Record<string, boolean> = {};
-
-  for (const { dataPath: path, message } of schema.errors) {
-    if (!message) {
-      continue;
-    }
-
-    const key = `${path}/${message}`;
-
-    if (hash[key]) {
-      continue;
-    }
-
-    errors.push({ message, path });
-
-    hash[key] = true;
-  }
-
-  return errors;
-};
-
 export const newValidate = (
   schema: ValidateFunction,
   data: JSONValue,
--- tests/validate.test.ts
+++ tests/validate.test.ts
@@ -1,58 +1,7 @@
 import { readSchema } from "../src/read";
-import { newValidate, validate } from "../src/validate";
+import { newValidate } from "../src/validate";
 
 import { getFixture, getFixturePath } from "./helpers";
-
-describe("validate", () => {
-  const schemaPath = getFixturePath("validate/validate/schema.json");
-
-  it("returns empty list of errors when data is valid", () => {
-    const schema = readSchema(schemaPath);
-    const data = getFixture("validate/validate/valid.json");
-
-    expect(validate(schema, data)).toEqual([]);
-  });
-
-  it("returns list of errors when data is invalid", () => {
-    const schema = readSchema(schemaPath);
-    const data = getFixture("validate/validate/invalid.json");
-
-    expect(validate(schema, data)).toEqual([
-      {
-        message: "should NOT have additional properties",
-        path: ""
-      },
-      {
-        message: "should be string",
-        path: ".foo"
-      },
-      {
-        message: "should be string",
-        path: ".bar"
-      }
-    ]);
-  });
-
-  it("returns list of unique errors when data is invalid", () => {
-    const schema = readSchema(schemaPath);
-    const data = getFixture("validate/validate/repeated.json");
-
-    expect(validate(schema, data)).toEqual([
-      {
-        message: "should NOT have additional properties",
-        path: ""
-      },
-      {
-        message: "should be string",
-        path: ".foo"
-      },
-      {
-        message: "should be string",
-        path: ".bar"
-      }
-    ]);
-  });
-});
 
 describe("newValidate", () => {
   describe("when data is valid", () => {
