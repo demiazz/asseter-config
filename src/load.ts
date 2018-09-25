e74bdf5999c95f9c4a43f9380aa2b4d7f83eecbd
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-25 11:34:42 +0300

    Replace `load` by `loadProvider`

--- src/load.ts
+++ src/load.ts
@@ -2,7 +2,7 @@ import camelCase from "camel-case";
 import constantCase from "constant-case";
 import { resolve } from "path";
 
-import { read, readSchema, readSchemas } from "./read";
+import { read, readSchema } from "./read";
 import { Configuration, JSONValue, RawConfiguration } from "./types";
 import { validate } from "./validate";
 import { ValidationError } from "./validation-error";
@@ -14,19 +14,12 @@ const isRawConfiguration = (
   return errors.length === 0;
 };
 
-const getCurrentEnvironment = ({
-  defaultEnvironment,
-  environmentVariable
-}: RawConfiguration): string => {
-  return process.env[environmentVariable] || defaultEnvironment;
-};
-
-export const loadProvider = (
+export const load = (
   configurationPath: string,
   providerType: string,
   looseSchemaPath: string,
   strictSchemaPath: string
-) => {
+): Configuration => {
   // 1. read data from a file.
 
   const data = read(configurationPath);
@@ -207,202 +200,6 @@ export const loadProvider = (
   }
 
   // 17. return result.
-
-  return data;
-};
-
-export const load = (
-  filePath: string,
-  providersSchemas?: Record<string, { loose: string; strict: string }>
-): Configuration => {
-  // Step 1: read data from a file.
-
-  const data: JSONValue = read(filePath);
-
-  // Step 2: validate with root schema.
-
-  const rootSchema = readSchema(resolve(__dirname, "../schema.json"));
-  const rootErrors = validate(rootSchema, data);
-
-  if (!isRawConfiguration(data, rootErrors)) {
-    throw new ValidationError(rootErrors);
-  }
-
-  // Step 3: validate with providers schemas.
-
-  const providersNames = Object.keys(data.providers);
-
-  if (providersSchemas) {
-    const looseSchemas = Object.keys(providersSchemas).reduce<
-      Record<string, string>
-    >((result, providerName) => {
-      result[providerName] = providersSchemas[providerName].loose;
-
-      return result;
-    }, {});
-    const schemas = readSchemas(looseSchemas);
-    const providersErrors: string[] = [];
-
-    for (const providerName of providersNames) {
-      const provider = data.providers[providerName];
-      const schema = schemas[provider.type];
-
-      if (!schema) {
-        continue;
-      }
-
-      providersErrors.push(
-        ...validate(
-          schema,
-          provider.options,
-          `providers.${providerName}.options`
-        )
-      );
-
-      if (!provider.environment) {
-        continue;
-      }
-
-      const environments = Object.keys(provider.environment);
-
-      for (const environment of environments) {
-        providersErrors.push(
-          ...validate(
-            schema,
-            provider.environment[environment],
-            `providers.${providerName}.environment.${environment}`
-          )
-        );
-      }
-    }
-
-    if (providersErrors.length > 0) {
-      throw new ValidationError(providersErrors);
-    }
-  }
-
-  // Step 4: merge options based on environment.
-
-  const currentEnvironment = getCurrentEnvironment(data);
-
-  for (const providerName of providersNames) {
-    const provider = data.providers[providerName];
-
-    if (!provider.environment) {
-      continue;
-    }
-
-    const environmentOptions = provider.environment[currentEnvironment];
-
-    delete provider.environment;
-
-    if (environmentOptions) {
-      provider.options = { ...provider.options, ...environmentOptions };
-    }
-  }
-
-  // Step 5: validate with providers schemas.
-
-  if (providersSchemas) {
-    const strictSchemas = Object.keys(providersSchemas).reduce<
-      Record<string, string>
-    >((result, providerName) => {
-      result[providerName] = providersSchemas[providerName].strict;
-
-      return result;
-    }, {});
-    const schemas = readSchemas(strictSchemas);
-    const providersErrors: string[] = [];
-
-    for (const providerName of providersNames) {
-      const provider = data.providers[providerName];
-      const schema = schemas[provider.type];
-
-      if (!schema) {
-        continue;
-      }
-
-      providersErrors.push(
-        ...validate(
-          schema,
-          provider.options,
-          `providers.${providerName}.options`
-        )
-      );
-    }
-
-    if (providersErrors.length > 0) {
-      throw new ValidationError(providersErrors);
-    }
-  }
-
-  // Step 6: merge options from environment variables.
-
-  for (const providerName of providersNames) {
-    const options = data.providers[providerName].options;
-    const prefix = `ASSETER_${constantCase(providerName)}_`;
-
-    Object.keys(process.env).forEach(variable => {
-      if (!variable.startsWith(prefix)) {
-        return;
-      }
-
-      const value = process.env[variable];
-
-      if (value == null) {
-        return;
-      }
-
-      const name = camelCase(variable.substr(prefix.length));
-
-      if (value === "true") {
-        options[name] = true;
-      } else if (value === "false") {
-        options[name] = false;
-      } else if (value === "null") {
-        options[name] = null;
-      } else if (/^\d+$/.test(value)) {
-        options[name] = parseInt(value, 10);
-      } else {
-        options[name] = value;
-      }
-    });
-  }
-
-  // Step 7: validate with providers schema.
-
-  if (providersSchemas) {
-    const strictSchemas = Object.keys(providersSchemas).reduce<
-      Record<string, string>
-    >((result, providerName) => {
-      result[providerName] = providersSchemas[providerName].strict;
-
-      return result;
-    }, {});
-    const schemas = readSchemas(strictSchemas);
-    const providersErrors: string[] = [];
-
-    for (const providerName of providersNames) {
-      const provider = data.providers[providerName];
-      const schema = schemas[provider.type];
-
-      if (!schema) {
-        continue;
-      }
-
-      providersErrors.push(
-        ...validate(
-          schema,
-          provider.options,
-          `providers.${providerName}.options`
-        )
-      );
-    }
-
-    if (providersErrors.length > 0) {
-      throw new ValidationError(providersErrors);
-    }
-  }
 
   return data;
 };
--- tests/load.test.ts
+++ tests/load.test.ts
@@ -1,183 +1,11 @@
-import { load, loadProvider } from "../src/load";
+import { load } from "../src/load";
 
 import { getFixture, getFixturePath, setEnvironment } from "./helpers";
 
 describe("load", () => {
-  it("reads configuration from a file", () => {
-    const fixturePath = getFixturePath("load/load/read/actual.json");
-    const expected = getFixture("load/load/read/expected.json");
-
-    expect(load(fixturePath)).toEqual(expected);
-  });
-
-  describe("validates root configuration", () => {
-    it("throws an error", () => {
-      const fixturePath = getFixturePath(
-        "load/load/root-validation/actual.json"
-      );
-
-      expect(() => load(fixturePath)).toThrowErrorMatchingSnapshot();
-    });
-  });
-
-  describe("validates providers options before processing", () => {
-    it("throws an error", () => {
-      const rollupSchemaPath = getFixturePath(
-        "load/load/loose-providers-validation/rollup-schema.json"
-      );
-      const webpackSchemaPath = getFixturePath(
-        "load/load/loose-providers-validation/webpack-schema.json"
-      );
-      const fixturePath = getFixturePath(
-        "load/load/loose-providers-validation/actual.json"
-      );
-
-      expect(() =>
-        load(fixturePath, {
-          rollup: { loose: rollupSchemaPath, strict: "" },
-          webpack: { loose: webpackSchemaPath, strict: "" }
-        })
-      ).toThrowErrorMatchingSnapshot();
-    });
-  });
-
-  describe("with an environment variable", () => {
-    let restoreEnvironment: () => void;
-
-    afterEach(() => {
-      if (restoreEnvironment) {
-        restoreEnvironment();
-      }
-    });
-
-    it("don't merge options if an environment variable isn't set", () => {
-      const fixturePath = getFixturePath("load/load/environment/actual.json");
-      const expected = getFixture(
-        "load/load/environment/expected-default.json"
-      );
-
-      expect(load(fixturePath)).toEqual(expected);
-    });
-
-    it("merge options if an environment variable is set", () => {
-      restoreEnvironment = setEnvironment({ ASSETER_ENV: "production" });
-
-      const fixturePath = getFixturePath("load/load/environment/actual.json");
-      const expected = getFixture(
-        "load/load/environment/expected-production.json"
-      );
-
-      expect(load(fixturePath)).toEqual(expected);
-    });
-  });
-
-  describe("validates providers options after merging options", () => {
-    it("throws an error", () => {
-      const looseRollupSchemaPath = getFixturePath(
-        "load/load/strict-providers-validation/loose-rollup-schema.json"
-      );
-      const strictRollupSchemaPath = getFixturePath(
-        "load/load/strict-providers-validation/strict-rollup-schema.json"
-      );
-      const looseWebpackSchemaPath = getFixturePath(
-        "load/load/strict-providers-validation/loose-webpack-schema.json"
-      );
-      const strictWebpackSchemaPath = getFixturePath(
-        "load/load/strict-providers-validation/strict-webpack-schema.json"
-      );
-      const fixturePath = getFixturePath(
-        "load/load/strict-providers-validation/actual.json"
-      );
-
-      expect(() =>
-        load(fixturePath, {
-          rollup: {
-            loose: looseRollupSchemaPath,
-            strict: strictRollupSchemaPath
-          },
-          webpack: {
-            loose: looseWebpackSchemaPath,
-            strict: strictWebpackSchemaPath
-          }
-        })
-      ).toThrowErrorMatchingSnapshot();
-    });
-  });
-
-  describe("with environment variables", () => {
-    let restoreEnvironment: () => void;
-
-    afterEach(() => {
-      if (restoreEnvironment) {
-        restoreEnvironment();
-      }
-    });
-
-    it("merge options from environment variables", () => {
-      restoreEnvironment = setEnvironment(getFixture(
-        "load/load/environment-options/environment.json"
-      ) as Record<string, string>);
-      const fixturePath = getFixturePath(
-        "load/load/environment-options/actual.json"
-      );
-      const expected = getFixture(
-        "load/load/environment-options/expected.json"
-      );
-
-      expect(load(fixturePath)).toEqual(expected);
-    });
-  });
-
-  describe("validates providers options after merging environment options", () => {
-    let restoreEnvironment: () => void;
-
-    afterEach(() => {
-      if (restoreEnvironment) {
-        restoreEnvironment();
-      }
-    });
-
-    it("throws an error", () => {
-      restoreEnvironment = setEnvironment(getFixture(
-        "load/load/strict-environment-validation/environment.json"
-      ) as Record<string, string>);
-
-      const looseRollupSchemaPath = getFixturePath(
-        "load/load/strict-environment-validation/loose-rollup-schema.json"
-      );
-      const strictRollupSchemaPath = getFixturePath(
-        "load/load/strict-environment-validation/strict-rollup-schema.json"
-      );
-      const looseWebpackSchemaPath = getFixturePath(
-        "load/load/strict-environment-validation/loose-webpack-schema.json"
-      );
-      const strictWebpackSchemaPath = getFixturePath(
-        "load/load/strict-environment-validation/strict-webpack-schema.json"
-      );
-      const fixturePath = getFixturePath(
-        "load/load/strict-environment-validation/actual.json"
-      );
-
-      expect(() =>
-        load(fixturePath, {
-          rollup: {
-            loose: looseRollupSchemaPath,
-            strict: strictRollupSchemaPath
-          },
-          webpack: {
-            loose: looseWebpackSchemaPath,
-            strict: strictWebpackSchemaPath
-          }
-        })
-      ).toThrowErrorMatchingSnapshot();
-    });
-  });
-});
-
-describe("loadProvider", () => {
-  const data = getFixturePath("load/load-provider/data.json");
-  const looseSchema = getFixturePath("load/load-provider/loose-schema.json");
-  const strictSchema = getFixturePath("load/load-provider/strict-schema.json");
+  const data = getFixturePath("load/data.json");
+  const looseSchema = getFixturePath("load/loose-schema.json");
+  const strictSchema = getFixturePath("load/strict-schema.json");
 
   let restoreEnvironment: void | (() => void);
 
@@ -190,20 +18,16 @@ describe("loadProvider", () => {
   });
 
   it("reads configuration from a file", () => {
-    const expected = getFixture("load/load-provider/read/expected.json");
+    const expected = getFixture("load/read/expected.json");
 
-    expect(loadProvider(data, "webpack", looseSchema, strictSchema)).toEqual(
-      expected
-    );
+    expect(load(data, "webpack", looseSchema, strictSchema)).toEqual(expected);
   });
 
   describe("root validation", () => {
     it("throws an error when configuration is invalid", () => {
-      const actual = getFixturePath(
-        "load/load-provider/root-validation/actual.json"
-      );
+      const actual = getFixturePath("load/root-validation/actual.json");
       expect(() =>
-        loadProvider(actual, "webpack", looseSchema, strictSchema)
+        load(actual, "webpack", looseSchema, strictSchema)
       ).toThrowErrorMatchingSnapshot();
     });
   });
@@ -211,11 +35,11 @@ describe("loadProvider", () => {
   describe("loose validation", () => {
     it("throws an error when data is invalid", () => {
       const actualLooseSchema = getFixturePath(
-        "load/load-provider/loose-validation/loose-schema.json"
+        "load/loose-validation/loose-schema.json"
       );
 
       expect(() => {
-        loadProvider(data, "webpack", actualLooseSchema, strictSchema);
+        load(data, "webpack", actualLooseSchema, strictSchema);
       }).toThrowErrorMatchingSnapshot();
     });
   });
@@ -223,13 +47,11 @@ describe("loadProvider", () => {
   describe("options merge", () => {
     describe("when environment variable do not exist", () => {
       it("merge options based on default environment", () => {
-        const expected = getFixture(
-          "load/load-provider/options-merge/default-expected.json"
-        );
+        const expected = getFixture("load/options-merge/default-expected.json");
 
-        expect(
-          loadProvider(data, "webpack", looseSchema, strictSchema)
-        ).toEqual(expected);
+        expect(load(data, "webpack", looseSchema, strictSchema)).toEqual(
+          expected
+        );
       });
     });
 
@@ -240,12 +62,12 @@ describe("loadProvider", () => {
         });
 
         const expected = getFixture(
-          "load/load-provider/options-merge/variable-expected.json"
+          "load/options-merge/variable-expected.json"
         );
 
-        expect(
-          loadProvider(data, "webpack", looseSchema, strictSchema)
-        ).toEqual(expected);
+        expect(load(data, "webpack", looseSchema, strictSchema)).toEqual(
+          expected
+        );
       });
     });
   });
@@ -257,11 +79,11 @@ describe("loadProvider", () => {
       });
 
       const actual = getFixturePath(
-        "load/load-provider/options-strict-validation/actual.json"
+        "load/options-strict-validation/actual.json"
       );
 
       expect(() => {
-        loadProvider(actual, "webpack", looseSchema, strictSchema);
+        load(actual, "webpack", looseSchema, strictSchema);
       }).toThrowErrorMatchingSnapshot();
     });
   });
@@ -269,34 +91,30 @@ describe("loadProvider", () => {
   describe("environment merge", () => {
     it("merges environment variables as options", () => {
       restoreEnvironment = setEnvironment(getFixture(
-        "load/load-provider/environment-merge/environment.json"
+        "load/environment-merge/environment.json"
       ) as Record<string, string>);
 
       const emptySchema = getFixturePath(
-        "load/load-provider/environment-merge/empty-schema.json"
+        "load/environment-merge/empty-schema.json"
       );
-      const expected = getFixture(
-        "load/load-provider/environment-merge/expected.json"
-      );
+      const expected = getFixture("load/environment-merge/expected.json");
 
-      expect(loadProvider(data, "webpack", emptySchema, emptySchema)).toEqual(
-        expected
-      );
+      expect(load(data, "webpack", emptySchema, emptySchema)).toEqual(expected);
     });
   });
 
   describe("strict validation after environment merge", () => {
     it("merges environment variables as options", () => {
       restoreEnvironment = setEnvironment(getFixture(
-        "load/load-provider/environment-strict-validation/environment.json"
+        "load/environment-strict-validation/environment.json"
       ) as Record<string, string>);
 
       const emptySchema = getFixturePath(
-        "load/load-provider/environment-strict-validation/empty-schema.json"
+        "load/environment-strict-validation/empty-schema.json"
       );
 
       expect(() => {
-        loadProvider(data, "webpack", emptySchema, strictSchema);
+        load(data, "webpack", emptySchema, strictSchema);
       }).toThrowErrorMatchingSnapshot();
     });
   });
