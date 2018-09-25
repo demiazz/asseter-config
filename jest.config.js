af227514f9ce8734aa2319420be6b32bb3b2d141
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-25 11:42:29 +0300

    Move Jest config to `package.json`

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
-  testMatch: [
-    "<rootDir>/tests/**/*.test.ts"
-  ],
-  moduleFileExtensions: ["ts", "js", "json", "node"]
-};
