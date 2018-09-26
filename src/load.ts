b9655fd91a8de4f6c29a5563e58bd472defcf2fa
Alexey Plutalov <demiazz.py@gmail.com>
2018-09-26 10:29:52 +0300

    Split `load` function on small pieces

--- src/load.ts
+++ src/load.ts
@@ -7,11 +7,37 @@ import { Configuration, JSONValue, RawConfiguration } from "./types";
 import { validate } from "./validate";
 import { ValidationError } from "./validation-error";
 
+const rootValidate = (data: JSONValue): RawConfiguration => {
+  const schema = readSchema(resolve(__dirname, "../schema.json"));
+  const errors = validate(schema, data);
+
+  if (!isRawConfiguration(data, errors)) {
+    throw new ValidationError(errors);
+  }
+
+  return data as RawConfiguration;
+};
+
 const isRawConfiguration = (
   _: JSONValue,
   errors: string[]
 ): _ is RawConfiguration => {
   return errors.length === 0;
+};
+
+const filterProviders = (
+  configuration: RawConfiguration,
+  allowedType: string
+): void => {
+  const allProvidersNames = Object.keys(configuration.providers);
+
+  for (const providerName of allProvidersNames) {
+    const { type } = configuration.providers[providerName];
+
+    if (type !== allowedType) {
+      delete configuration.providers[providerName];
+    }
+  }
 };
 
 const looseValidate = (
@@ -50,6 +76,35 @@ const looseValidate = (
   }
 };
 
+const mergeOptions = ({
+  defaultEnvironment,
+  environmentVariable,
+  providers
+}: RawConfiguration): void => {
+  const currentEnvironment =
+    process.env[environmentVariable] || defaultEnvironment;
+
+  const providersNames = Object.keys(providers);
+
+  for (const providerName of providersNames) {
+    const provider = providers[providerName];
+
+    if (!provider.environment) {
+      continue;
+    }
+
+    const overrides = provider.environment[currentEnvironment];
+
+    if (!overrides) {
+      continue;
+    }
+
+    provider.options = { ...provider.options, ...overrides };
+
+    delete provider.environment;
+  }
+};
+
 const strictValidate = (
   { providers }: RawConfiguration,
   schema: ValidateFunction
@@ -70,6 +125,17 @@ const strictValidate = (
   }
 };
 
+const mergeFromEnvironment = ({ providers }: RawConfiguration): void => {
+  const providersNames = Object.keys(providers);
+
+  for (const providerName of providersNames) {
+    const provider = providers[providerName];
+    const environmentOptions = extractFromEnvironment(providerName);
+
+    provider.options = { ...provider.options, ...environmentOptions };
+  }
+};
+
 export const load = (
   configurationPath: string,
   providerType: string,
@@ -77,61 +143,16 @@ export const load = (
   strictSchemaPath: string
 ): Configuration => {
   const data = read(configurationPath);
-  const rootSchema = readSchema(resolve(__dirname, "../schema.json"));
   const looseSchema = readSchema(looseSchemaPath);
   const strictSchema = readSchema(strictSchemaPath);
+  const configuration = rootValidate(data);
 
-  const rootErrors = validate(rootSchema, data);
+  filterProviders(configuration, providerType);
+  looseValidate(configuration, looseSchema);
+  mergeOptions(configuration);
+  strictValidate(configuration, strictSchema);
+  mergeFromEnvironment(configuration);
+  strictValidate(configuration, strictSchema);
 
-  if (!isRawConfiguration(data, rootErrors)) {
-    throw new ValidationError(rootErrors);
-  }
-
-  const allProvidersNames = Object.keys(data.providers);
-
-  for (const providerName of allProvidersNames) {
-    const { type } = data.providers[providerName];
-
-    if (type !== providerType) {
-      delete data.providers[providerName];
-    }
-  }
-
-  looseValidate(data, looseSchema);
-
-  const currentEnvironment =
-    process.env[data.environmentVariable] || data.defaultEnvironment;
-
-  const providersNames = Object.keys(data.providers);
-
-  for (const providerName of providersNames) {
-    const provider = data.providers[providerName];
-
-    if (!provider.environment) {
-      continue;
-    }
-
-    const overrides = provider.environment[currentEnvironment];
-
-    if (!overrides) {
-      continue;
-    }
-
-    provider.options = { ...provider.options, ...overrides };
-
-    delete provider.environment;
-  }
-
-  strictValidate(data, strictSchema);
-
-  for (const providerName of providersNames) {
-    const provider = data.providers[providerName];
-    const environmentOptions = extractFromEnvironment(providerName);
-
-    provider.options = { ...provider.options, ...environmentOptions };
-  }
-
-  strictValidate(data, strictSchema);
-
-  return data;
+  return configuration;
 };
